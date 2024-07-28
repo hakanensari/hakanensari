09c024c2a5ab552379bc6a626b90603dcb70ddb2
Hakan Ensari <hakanensari@gmail.com>
2024-07-28 16:18:40 +0200

    Refactor js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,34 +1,42 @@
+const CANVAS_WIDTH = 270;
+const MARGIN_BOTTOM = 16;
+const MAX_SLICES = 15;
+
 const images = ["images/index/IMG_3565.jpg"];
 const image = new Image();
+
 image.src = images[Math.floor(Math.random() * images.length)];
 image.onload = () => {
   const canvas = document.createElement("canvas");
   document.querySelector("#images").append(canvas);
+  const context = canvas.getContext("2d");
+
   const multiply = () => {
     const aspectRatio = image.height / image.width;
-    canvas.width = 270;
+    canvas.width = CANVAS_WIDTH;
     canvas.height = canvas.width * aspectRatio;
-    canvas.style.marginBottom = "16px";
-    const count = Math.ceil(Math.random() * 15);
+    canvas.style.marginBottom = `${MARGIN_BOTTOM}px`;
+
+    const count = Math.ceil(Math.random() * MAX_SLICES);
     const slices = [];
-    canvas.getContext("2d").drawImage(image, 0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
     const width = canvas.width / count;
     const indices = [...Array(count).keys()];
+
     while (indices.length !== 0) {
       const index = indices.splice(
         Math.floor(Math.random() * indices.length),
         1
       );
-      slices.push(
-        canvas
-          .getContext("2d")
-          .getImageData(width * index, 0, width, canvas.height)
-      );
+      slices.push(context.getImageData(width * index, 0, width, canvas.height));
     }
+
     slices.forEach((slice, index) => {
-      canvas.getContext("2d").putImageData(slice, width * index, 0);
+      context.putImageData(slice, width * index, 0);
     });
+
     setTimeout(multiply, Math.random() * 50);
   };
+
   multiply();
 };
